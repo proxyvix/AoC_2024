b207f870c9f4cd560d209604d4001dff8fa3b00b
proxyvix <richard.csanadi@gmail.com>
2024-12-03 19:23:13 +0100

    day3 solutions

---
+++ day3/day3.go
@@ -0,0 +1,88 @@
+package day3
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func readData(path string) string {
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Printf("Error opening the file: %v", err)
+	}
+	defer file.Close()
+
+	var corruptedMemory string
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		corruptedMemory += scanner.Text()
+	}
+
+	return corruptedMemory
+}
+
+func calcMul(mul string) int {
+	cutPref, _ := strings.CutPrefix(mul, "mul(")
+	cutSuff, _ := strings.CutSuffix(cutPref, ")")
+
+	nums := strings.Split(cutSuff, ",")
+
+	num1, _ := strconv.Atoi(nums[0])
+	num2, _ := strconv.Atoi(nums[1])
+
+	val := num1 * num2
+
+	return val
+}
+
+func partOne(path string) int {
+	corruptedMemory := readData(path)
+
+	r, _ := regexp.Compile(`mul\(\d+,\d+\)`)
+	matches := r.FindAllString(corruptedMemory, -1)
+
+	var totalSum int
+	for _, match := range matches {
+		totalSum += calcMul(match)
+	}
+
+	return totalSum
+}
+
+func partTwo(path string) int {
+	corruptedMemory := readData(path)
+
+	r, _ := regexp.Compile(`mul\(\d+,\d+\)|don't\(\)|do\(\)`)
+	matches := r.FindAllString(corruptedMemory, -1)
+
+	enabled := true
+	var totalSum int
+	for _, match := range matches {
+		if match == "don't()" {
+			enabled = false
+			continue
+		}
+		if match == "do()" {
+			enabled = true
+			continue
+		}
+		if enabled {
+			totalSum += calcMul(match)
+		}
+	}
+
+	return totalSum
+}
+
+func DayThree() {
+	partOne := partOne("day3/day3.txt")
+	fmt.Printf("Solution for day 3 part one: %d\n", partOne)
+
+	partTwo := partTwo("day3/day3.txt")
+	fmt.Printf("Solution for day 3 part two: %d\n", partTwo)
+}
--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"github.com/proxyvix/AoC_2024/day1"
 	"github.com/proxyvix/AoC_2024/day2"
+	"github.com/proxyvix/AoC_2024/day3"
 )
 
 func main() {
 	day1.DayOne()
 	day2.DayTwo()
+	day3.DayThree()
 }
